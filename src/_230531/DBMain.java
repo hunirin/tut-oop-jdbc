0cbfc0012277d6963ea5b2e99956fdf9df840889
hun053@naver.com <hun053@naver.com>
2023-05-31 14:20:39 +0900

    feat: Person 전체조회

--- src/_230531/DBMain.java
+++ src/_230531/DBMain.java
@@ -1,92 +1,96 @@
 package _230531;
 
+import _230531.model.BassDAO;
+import _230531.model.Person;
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DBMain {
+public class DBMain extends BassDAO {
     public static void main(String[] args) {
-        String sql = "select 1+1 as result";
-        initPerson();
+        DBMain dbMain = new DBMain();
+        dbMain.initPerson();
+        System.out.println(dbMain.insertPerson("leo"));
+        System.out.println(dbMain.insertPerson("yui"));
+        List<Person> personList = dbMain.findAllPerson();
+        System.out.println(personList.toString());
     }
 
-    private static void initPerson() {
-        Connection conn = null;
-        Statement smt = null;
-        PreparedStatement psmt = null;
-        ResultSet rs = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:sqlite:world.db");
-            smt = conn.createStatement();
-            smt.setQueryTimeout(30);  // set timeout to 30 sec.
-
-            smt.executeUpdate("drop table if exists person");
-            smt.executeUpdate("create table person (id integer, name string)");
-            smt.executeUpdate("insert into person values(1, 'leo')");
-            smt.executeUpdate("insert into person values(2, 'yui')");
-            rs = smt.executeQuery("select * from person");
-            while(rs.next()) {
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
-            }
-        }
-        catch(SQLException e) {
-            System.err.println(e.getMessage());
-        }
-        finally {
-            try {
-                if (rs != null) rs.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                if (smt != null) smt.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                if (psmt != null) psmt.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                if (conn != null) conn.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private static void gatCodeName() {
-        String sql = "select code, name from country order by code, name";
-        Connection conn = null;
-        PreparedStatement psmt = null;
-        ResultSet rs = null;
+    private List<Person> findAllPerson() {
+        List<Person> result = new ArrayList<>();
+        String sql = "select id, name from person";
         try {
             conn = DriverManager.getConnection("jdbc:sqlite:world.db");
             psmt = conn.prepareStatement(sql);
             rs = psmt.executeQuery();
             while( rs.next() ) {
-                System.out.println( rs.getString("result"));
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                result.add(new Person(id, name));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+        return result;
+    }
+
+    private int insertPerson(String name) {
+        int cnt = 0;
+        String sql = "insert into person(name) values(?)";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, name);
+            cnt = psmt.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
+        return cnt;
+    }
+
+
+    private void initPerson() {
+        String sql = """
+                create table person (
+                id integer primary key autoincrement,
+                name string
+                )
+                """;
+        try {
+            getConn();
+            smt = conn.createStatement();
+            smt.executeUpdate("drop table if exists person");
+            smt.executeUpdate(sql);
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
+    }
+
+    private void getCodeName() {
+        String sql = "select code, name from country order by code, name";
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:world.db");
+            psmt = conn.prepareStatement(sql);
+            rs = psmt.executeQuery();
+            while( rs.next() ) {
                 System.out.print( rs.getString("code") + "\t");
                 System.out.println( rs.getString("name"));
             }
-    } catch (SQLException e) {
-        e.printStackTrace();
-    } finally {
-        try {
-            if (rs != null) rs.close();
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-        }
-        try {
-            if (psmt != null) psmt.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            if (conn != null) conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } finally {
+            close();
         }
     }
-}
 }
